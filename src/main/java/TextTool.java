fc49fe7b8e95523716cf4fbd6d8c0789d1d80870
Tim Lewis <tim@phurba.net>
2018-03-23 23:53:19 +1300

    Add ability to generate docx files.

--- src/main/java/TextTool.java
+++ src/main/java/TextTool.java
@@ -1,11 +1,8 @@
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -13,19 +10,18 @@ import java.util.concurrent.TimeUnit;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
-import org.apache.jena.base.Sys;
 
-class EpubRunnable implements Runnable {
-    private EpubGenerator epubGenerator;
+class DocumentRunnable implements Runnable {
+    private DocumentGenerator documentGenerator;
 
-    EpubRunnable(EpubGenerator epubGenerator) {
-        this.epubGenerator = epubGenerator;
+    DocumentRunnable(DocumentGenerator documentGenerator) {
+        this.documentGenerator = documentGenerator;
 
     }
 
     @Override
     public void run() {
-        epubGenerator.generateEpub();
+        documentGenerator.generateDocuments(true, false);
     }
 }
 
@@ -39,8 +35,8 @@ class Args {
     @Parameter(names={"--outputDir", "-o"}, order = 1, description = "The directory where the generated files will be saved. Defaults to ./output")
     public String outputDir;
 
-    @Parameter(names={"--epubFiles", "-ef"}, order = 2, description = "The directory that contains files used for the epub. Defaults to ./epub_files")
-    public String epubFiles;
+    @Parameter(names={"--documentFiles", "-df"}, order = 2, description = "The directory that contains files used for the epub and docx. Defaults to ./document_files")
+    public String documentFiles;
 
     @Parameter(names={"--itemId", "-id"}, order = 3, description = "If supplied, only the item with this id will be processed.")
     public String itemId;
@@ -48,7 +44,10 @@ class Args {
     @Parameter(names={"--titleAsFilename", "-t"}, order = 4, description = "Use the text title as the filename. Otherwise, the text's ID is used.")
     public boolean titleAsFilename;
 
-    @Parameter(names={"--help", "-h"}, order = 5, help = true, description = "Display the usage information.")
+    @Parameter(names={"--docx", "-d"}, order = 5, description = "Generate docx files in addition to the epubs")
+    public boolean docx;
+
+    @Parameter(names={"--help", "-h"}, order = 6, help = true, description = "Display the usage information.")
     public boolean help;
 }
 
@@ -83,8 +82,8 @@ public class TextTool {
         int processors = Runtime.getRuntime().availableProcessors();
         threadCount = (processors > 1) ? processors - 1 : 1;
 
-        String workingDir = ensureTrailingSlash(System.getProperty("user.dir"));
-        String dataPath = ensureTrailingSlash(commandArgs.sourceDir);
+        String workingDir = StringUtils.ensureTrailingSlash(System.getProperty("user.dir"));
+        String dataPath = StringUtils.ensureTrailingSlash(commandArgs.sourceDir);
         if (!(new File(dataPath).exists())) {
             System.out.println("Error: Supplied source directory does not exist - " + dataPath);
             return;
@@ -97,26 +96,26 @@ public class TextTool {
             }
             outputDirPath = commandArgs.outputDir;
         }
-        outputDirPath = ensureTrailingSlash(outputDirPath) + getOutputDirName();
-        String epubFilesDir = workingDir + "epub_files";
-        if (commandArgs.epubFiles != null) {
-            epubFilesDir = ensureTrailingSlash(commandArgs.epubFiles) + "epub_files";
+        outputDirPath = StringUtils.ensureTrailingSlash(outputDirPath) + getOutputDirName();
+        String documentFilesDir = workingDir + "document_files";
+        if (commandArgs.documentFiles != null) {
+            documentFilesDir = StringUtils.ensureTrailingSlash(commandArgs.documentFiles) + "document_files";
         }
         String itemId = commandArgs.itemId;
 
         if (itemId != null && itemId.length() > 0) {
             // just process the given item
-            processResource(itemId, dataPath, outputDirPath, epubFilesDir, null);
+            processResource(itemId, dataPath, outputDirPath, documentFilesDir, commandArgs.docx, null);
         } else {
-            createEpubsForDirectory(dataPath, outputDirPath, epubFilesDir);
+            createEpubsForDirectory(dataPath, outputDirPath, documentFilesDir, commandArgs.docx);
         }
     }
 
-    private static void createEpubsForDirectory(String sourceDir, String outputDir, String epubFilesDir)
+    private static void createEpubsForDirectory(String sourceDir, String outputDir, String epubFilesDir, boolean createDocx)
     {
         ExecutorService executor = Executors.newFixedThreadPool(threadCount);
 
-        String itemsPath = ensureTrailingSlash(sourceDir) + "items";
+        String itemsPath = StringUtils.ensureTrailingSlash(sourceDir) + "items";
         File[] dirs = new File(itemsPath).listFiles();
         List<String> etextItemPaths = new ArrayList<>();
         for (File dir: dirs) {
@@ -133,7 +132,7 @@ public class TextTool {
                         }
 
                         etextItemPaths.add(item.getAbsolutePath());
-                        processResource(id, sourceDir, outputDir, epubFilesDir, executor);
+                        processResource(id, sourceDir, outputDir, epubFilesDir, createDocx, executor);
                     }
                 }
             }
@@ -147,40 +146,15 @@ public class TextTool {
         }
     }
 
-    private static void processResource(String id, String sourceDir, String outputDir, String epubFilesDir, ExecutorService executor)
+    private static void processResource(String id, String sourceDir, String outputDir, String epubFilesDir, boolean createDocx, ExecutorService executor)
     {
-        EpubGenerator epubGenerator = new EpubGenerator(id, sourceDir, outputDir, epubFilesDir, titleAsFilename);
+        DocumentGenerator documentGenerator = new DocumentGenerator(id, sourceDir, outputDir, epubFilesDir, titleAsFilename);
         if (executor == null) {
-            epubGenerator.generateEpub();
+            documentGenerator.generateDocuments(true, createDocx);
         } else {
-            EpubRunnable runnable = new EpubRunnable(epubGenerator);
+            DocumentRunnable runnable = new DocumentRunnable(documentGenerator);
             executor.execute(runnable);
         }
-    }
-
-    private static boolean fileContainsString(String filepath, String string)
-    {
-        File file = new File(filepath);
-        boolean containsString = false;
-        try {
-            Scanner scanner = new Scanner(file);
-            while(scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                if (line.contains(string)) {
-                    containsString = true;
-                    break;
-                }
-            }
-        } catch(Exception e) {
-            System.out.println("Error searching file for string: " + filepath);
-        }
-
-        return containsString;
-    }
-
-    private static String ensureTrailingSlash(String path)
-    {
-        return path.endsWith("/") ? path : path + "/";
     }
 
     private static String getOutputDirName()
